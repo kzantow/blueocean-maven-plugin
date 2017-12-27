964d0f121989fc43ed16ce22f72ff3ff79da5916
Keith Zantow <kzantow@gmail.com>
2017-12-27 14:57:47 -0500

    Move order-dependent integration tests to aggregator

--- src/it/it-aggregator/downstream-plugin/src/test/java/io/jenkins/blueocean/maven/plugin/TestBlueOceanDependencies.java
+++ src/it/it-aggregator/downstream-plugin/src/test/java/io/jenkins/blueocean/maven/plugin/TestBlueOceanDependencies.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.jenkins.blueocean.maven.plugin;
 
 import java.io.File;
@@ -11,8 +6,7 @@ import java.util.List;
 import org.junit.Test;
 
 /**
- *
- * @author kzantow
+ * Validates all the expected files have been copied to the proper locations
  */
 public class TestBlueOceanDependencies {
     @Test
@@ -21,7 +15,7 @@ public class TestBlueOceanDependencies {
         File node_modules = new File(cwd, "node_modules");
         List<String> files = Arrays.asList(node_modules.list());
         System.out.println(node_modules.getPath() + " list: " + files);
-        assert files.size() > 0 : "Should have entries in node_modules";
+        Assert files.size() > 0 : "Should have entries in node_modules";
         assert files.contains("@jenkins-cd") : "Should have upstream-1 / @jenkins-cd";
         assert files.contains("upstream-2") : "Should have upstream-2";
     }
